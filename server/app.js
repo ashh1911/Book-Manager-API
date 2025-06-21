21a002210ba3d2581efb56111a73bb4403146151
Ashish Sharma <ashishhsharma19@gmail.com>
2025-06-21 18:31:29 +0530

    app.js

--- server/app.js
+++ server/app.js
@@ -5,17 +5,13 @@ require('dotenv').config();
 
 const app = express();
 
-// Connect to MongoDB
 connectDB();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Routes
 app.use('/api/books', require('./routes/Books'));
 
-// Server Listen
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
